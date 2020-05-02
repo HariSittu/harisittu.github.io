147523f1dc11fb112b4871855cb52da5fd2a6862
HariSittu <mailtohariharanv@gmail.com>
2020-05-02 13:17:29 +0530

    Updates

---
+++ precache-manifest.e2266d6bbc029205e34cea2457eaf754.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b7d858c36e12c4ff26668955fad7e0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b5883a6fca1de6b822c0",
+    "url": "/portfolio/static/css/2.ad458773.chunk.css"
+  },
+  {
+    "revision": "d3e9f011cec40abe4c95",
+    "url": "/portfolio/static/css/main.67e8c8ef.chunk.css"
+  },
+  {
+    "revision": "b5883a6fca1de6b822c0",
+    "url": "/portfolio/static/js/2.0dcadd3c.chunk.js"
+  },
+  {
+    "revision": "e38ca522e804e0607d4daf6f90448726",
+    "url": "/portfolio/static/js/2.0dcadd3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3e9f011cec40abe4c95",
+    "url": "/portfolio/static/js/main.d74e2473.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  }
+]);
